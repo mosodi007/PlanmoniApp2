eb971ed1e2ef91408d7b6d2c866a9cea88a2e72b
mosodi007 <martins.osodi@gmail.com>
2025-06-30 18:13:59 +0100

    Deleted env.d.ts

--- extracted-project/project/types/lib/streamChat.ts
+++
@@ -1,5 +0,0 @@
-import { StreamChat } from 'stream-chat';
-
-const API_KEY = 'bb4c2hy6axsb'; // TODO: Replace with your actual Stream API key
-
-export const chatClient = StreamChat.getInstance(API_KEY); 
